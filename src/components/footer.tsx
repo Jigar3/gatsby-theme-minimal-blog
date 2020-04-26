156d4b162ecd4c75f75e2cdd5c29bac6ebf13654
Jigar <chavada.jigar.3@gmail.com>
2020-04-27 00:06:05 +0530

    added code

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,8 @@
 /** @jsx jsx */
 import { jsx, Styled } from "theme-ui"
 import useSiteMetadata from "../hooks/use-site-metadata"
+import { FaLinkedin, FaTwitter, FaGithub, FaFacebook, FaInstagram } from "react-icons/fa";
+
 
 const Footer = () => {
   const { siteTitle } = useSiteMetadata()
@@ -9,8 +11,8 @@ const Footer = () => {
     <footer
       sx={{
         boxSizing: `border-box`,
-        display: `flex`,
-        justifyContent: `space-between`,
+        // display: `flex`,
+        // justifyContent: `space-between`,
         mt: [6],
         color: `secondary`,
         a: {
@@ -20,8 +22,27 @@ const Footer = () => {
         variant: `dividers.top`,
       }}
     >
-      <div>
-        &copy; {new Date().getFullYear()} by {siteTitle} | Theme By <a href="https://www.gatsbyjs.org/starters/LekoArts/gatsby-starter-minimal-blog/">LekoArts</a>
+        
+      <div
+        sx={{
+          display: `flex`,
+          justifyContent: `center`,
+          margin: '1rem 0 1rem 0',
+        }}
+      >
+        <a href="https://www.linkedin.com/in/jsc39/"><FaLinkedin size="32px" sx={{ mr: [4], ml: [3] }}/></a> 
+        <a href="https://twitter.com/JigarChavada1"><FaTwitter size="32px" sx={{ mr: [4] }}/></a> 
+        <a href="https://github.com/jigar3"><FaGithub size="32px" sx={{ mr: [4] }}/></a> 
+        <a href="https://www.instagram.com/jigar_03/"><FaInstagram size="32px" sx={{ mr: [4] }}/></a> 
+        <a href="https://www.facebook.com/jsc3998/"><FaFacebook size="32px" sx={{ mr: [4] }}/></a> 
+      </div>
+      
+      <div
+        sx={{
+          textAlign: `center`,
+          width: `100%`,
+        }}
+      > Theme By <a href="https://www.gatsbyjs.org/starters/LekoArts/gatsby-starter-minimal-blog/">LekoArts</a>
       </div>
     </footer>
   )
